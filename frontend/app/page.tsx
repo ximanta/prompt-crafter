3f97185df6a48ee61a8a1cfdd62f657739d90912
Simanta.Sarma <ximanta.sarma@gmail.com>
2024-09-05 23:27:09 +0530

    Updated view to look clean

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -2,10 +2,10 @@ import Image from "next/image";
 import { PromptEnhancer } from "@/components/prompt-enhancer";
 import { PromptEnhancerStream } from "@/components/prompt-enhancer-stream";
 import { PromptEnhancerSocket } from "@/components/prompt_enhancer_socket";
+
 export default function Home() {
   return (
-    
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex min-h-screen w-full flex-col items-center justify-between p-4">
       {/* <PromptEnhancer /> */}
       <PromptEnhancerStream />
       {/* <PromptEnhancerSocket /> */}
